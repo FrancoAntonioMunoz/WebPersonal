bfee3e401487a1085f870bfca2a9ce9187ba9e4d
FrancoAntonioMunoz <48764397+FrancoAntonioMunoz@users.noreply.github.com>
2019-04-18 22:42:45 -0400

    Llenar datos a admin

--- Pag/models.py
+++ Pag/models.py
@@ -7,7 +7,7 @@ class Pag(models.Model):
     name = models.CharField(max_length=100,null=True,blank=True,verbose_name='Titulo')
     headers=models.CharField(max_length=240,null=True,blank=True,verbose_name='Encabezado')
     content=models.TextField(max_length=500,null=True,blank=True,verbose_name='Contenido')
-    image=models.ImageField(upload_to=image_Pag)
+    image=models.ImageField(upload_to=image_Pag,null=True,blank=True)
     state=models.CharField(max_length=240,default="Activo")
     created= models.DateTimeField(auto_now_add=True,verbose_name='Fecha Creación')
     updated= models.DateTimeField(auto_now=True,verbose_name='Fecha Actualización')
